eb71e306c2ebd9f2ca1dec2a3d69025ebe4b1c19
horsjaco117 <jacob_horsley@live.com>
2025-01-16 08:50:02 -0700

    Add header, add sum operator example

--- OperatorsExample/OperatorsExample.vb
+++ OperatorsExample/OperatorsExample.vb
@@ -1,6 +1,14 @@
-﻿Module OperatorsExample
+﻿'Jacob Horsley
+'Spring 2025
+'RCET 2265
+'OperatorsExample
+'https://github.com/horsjaco117/OperatorsExample
+
+Module OperatorsExample
 
     Sub Main()
+        Console.Write("5 + 3 = ") 'Print a string
+        Console.WriteLine(5 + 3) 'print the sum
 
     End Sub
 
