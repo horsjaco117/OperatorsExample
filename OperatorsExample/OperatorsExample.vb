d46b87351b7d7fb9f434613a89403a1197c2b968
horsjaco117 <jacob_horsley@live.com>
2025-01-17 08:19:42 -0700

    Add more operator examples

--- OperatorsExample/OperatorsExample.vb
+++ OperatorsExample/OperatorsExample.vb
@@ -4,11 +4,35 @@
 'OperatorsExample
 'https://github.com/horsjaco117/OperatorsExample
 
+Imports System.Net.Http
+
 Module OperatorsExample
 
     Sub Main()
+
+        'sum operator
         Console.Write("5 + 3 = ") 'Print a string
         Console.WriteLine(5 + 3) 'print the sum
+        Console.WriteLine()
+
+
+        'subtraction(Difference) operator
+        Console.Write("5 - 3 = ") 'Print a string
+        Console.WriteLine(5 - 3) 'print the difference
+        Console.WriteLine()
+
+        'multiplication operator
+        Console.Write("5 * 3 = ") 'Print a string
+        Console.WriteLine(5 * 3) 'print the multiplication
+        Console.WriteLine()
+
+        'Floating point division operation
+        Console.Write("5 / 3 = ") 'Print a string
+        Console.WriteLine(5 / 3) 'print the quotient
+        Console.WriteLine()
+
+
+
 
     End Sub
 
